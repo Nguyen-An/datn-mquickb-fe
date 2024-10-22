aa077d13cef37388ade7b03369c9177de80f13ca
An Nguyen <vanan6b@gmail.com>
2024-10-22 21:40:32 +0700

    fix run build

--- src/api/hook.ts
+++ src/api/hook.ts
@@ -1,23 +1,24 @@
-import axios, { AxiosRequestConfig } from 'axios'
+import axios, { AxiosRequestConfig } from 'axios';
+
 const getAPIURL = (path: string) => {
-    return `${process.env.API_URL || 'http://localhost:8000/'
-        }${path}`
-}
+    return `${process.env.API_URL || 'http://localhost:8000/'}${path}`;
+};
 
 const instance = axios.create({
-    baseURL: process.env.API_URL
-})
+    baseURL: process.env.API_URL,
+});
 
-const sendGet = (url: string, params?: any) => instance.get(url, { params })
+// Thay thế `any` bằng `unknown` hoặc các kiểu cụ thể hơn
+const sendGet = (url: string, params?: Record<string, unknown>) => instance.get(url, { params });
 
-const sendGetFile = (url: string, params?: any) => instance.get(url, { params, responseType: 'blob' })
+const sendGetFile = (url: string, params?: Record<string, unknown>) => instance.get(url, { params, responseType: 'blob' });
 
-const sendPost = (url: string, params?: any, config?: AxiosRequestConfig) => {
-    return instance.post(url, params, config)
-}
+const sendPost = (url: string, params?: Record<string, unknown>, config?: AxiosRequestConfig) => {
+    return instance.post(url, params, config);
+};
 
-const sendPut = (url: string, params: any) => instance.put(url, params)
+const sendPut = (url: string, params: Record<string, unknown>) => instance.put(url, params);
 
-const sendDelete = (url: string, params?: any) => instance.delete(url, params)
+const sendDelete = (url: string, params?: Record<string, unknown>) => instance.delete(url, { data: params });
 
-export { sendGet, sendGetFile, sendPost, sendDelete, sendPut, instance, getAPIURL }
+export { sendGet, sendGetFile, sendPost, sendDelete, sendPut, instance, getAPIURL };
