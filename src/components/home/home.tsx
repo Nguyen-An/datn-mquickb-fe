196e3eb97bf941f288be377d46c9d963d143b2f2
An Nguyen <vanan6b@gmail.com>
2024-10-23 10:50:24 +0700

    update code

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -5,7 +5,6 @@ const Home = () => {
   return (
     <div style={{ padding: '20px' }}>
     <h1>Welcome to My Home Page</h1>
-    <p>This is a simple Next.js app using Ant Design for the menu.</p>
   </div>
   );
 };
