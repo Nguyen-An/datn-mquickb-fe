29acbad9f17593b68b97f0aade7c3ae70e8b9f4f
monly <vanan6b@gmail.com>
2024-10-24 17:20:54 +0700

    setup lib shadCn UI

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,11 +1,14 @@
 "use client";
 import React from 'react';
-
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 const Home = () => {
   return (
     <div style={{ padding: '20px' }}>
-    <h1>Welcome to My Home Page</h1>
-  </div>
+      <Button variant="outline">Button</Button>
+      <Input />
+      <h1>Welcome to My Home Page</h1>
+    </div>
   );
 };
 
