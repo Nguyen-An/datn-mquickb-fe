e9b66187750ec8da332ffe746413807e698429b7
monly <vanan6b@gmail.com>
2024-11-05 17:30:34 +0700

    update landing page

---
+++ public/icons/index.ts
@@ -0,0 +1,15 @@
+import iconBrownWatchIMG from './brown-watch.png'
+import iconCakeIMG from './cake.png'
+import iconChefAvatarIMG from './chef-avatar.png'
+import iconKitchenToolIMG from './kitchen-tools.png'
+import iconQRCodeIMG from './qrcode.png'
+import iconlogoIMG from './logo-round.png'
+
+export default {
+    iconBrownWatch: iconBrownWatchIMG.src,
+    iconCake: iconCakeIMG.src,
+    iconChefAvatar: iconChefAvatarIMG.src,
+    iconKitchenTool: iconKitchenToolIMG.src,
+    iconQRCode: iconQRCodeIMG.src,
+    iconlogo: iconlogoIMG.src,
+}
